312e3fec4053fcf8138f8acb7dba5e1aed306fec
Billal <bito-kai@hotmail.fr>
2025-10-03 08:52:59 +0200

    Accueil - Fleche Haut de Page - En cours

--- billaldev/js/script.js
+++ billaldev/js/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {});
+// === Bouton retour haut de page ===
 document.addEventListener("DOMContentLoaded", () => {
   console.log("✅ Script chargé !");
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  if (scrollToTopBtn) {
+    // Affiche/masque le bouton selon le scroll
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        scrollToTopBtn.classList.add("show");
+      } else {
+        scrollToTopBtn.classList.remove("show");
+      }
+    });
+
+    // Action du clic
+    scrollToTopBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+  }
 });
